72378680b330fc40e3b7f14a6496ab740e99edb8
sangrimlee <contact@sangrim.dev>
2022-01-05 17:14:38 +0900

    Project Setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-4xl mr-0 text-blue-700">Hello world!</h1>
+    </div>
+  );
+}
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  mode: isProduction ? 'production' : 'development',
+  entry: { app: path.join(__dirname, 'src/index.tsx') },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: isProduction ? '[contenthash:8].js' : '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/i,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: isProduction ? '[contenthash:8].css' : 'styles.css',
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      minify: isProduction,
+    }),
+  ],
+  devServer: {
+    compress: true,
+    historyApiFallback: true,
+    open: true,
+    port: 3000,
+  },
+  optimization: {
+    minimize: isProduction,
+  },
+};
