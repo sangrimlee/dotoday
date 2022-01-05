b262d3064984eea182113209807b32909898fb6c
sangrimlee <contact@sangrim.dev>
2022-01-05 23:02:20 +0900

    feat(style): 폰트 및 색상 변경
    기본 폰트, 로고 폰트 추가
    기본 색 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'class',
   theme: {
+    fontFamily: {
+      logo: ['Gilroy', 'system-ui'],
+      sans: ['"Noto Sans KR"', 'ui-sans-serif', 'system-ui'],
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.black,
+      neutral: colors.neutral,
+      red: '#FF4C4C',
+      brand: '#1CD265',
+    },
     extend: {},
   },
   plugins: [],
